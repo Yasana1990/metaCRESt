beaf9f7c13cde48e96930933c8d8b3ac077c7684
Andrew Igorevich Kropochev <kropochev@bionet.nsc.ru>
2021-10-19 14:15:01 +0700

    Upload New File

---
+++ TPMformater-def.py
@@ -0,0 +1,58 @@
+
+def formatting(markername):
+
+    f1 = open('rawTPM_' + markername + '.txt', 'r')
+    tpm_list = f1.read().splitlines()
+    
+    for i in range(0, len(tpm_list)):
+        tpm_list[i] = tpm_list[i].replace(':', '\t')
+        tpm_list[i] = tpm_list[i].split('\t')
+        
+    f1.close
+    
+    f2 = open('ContigID_'+markername+'_dict.txt', 'r')
+    dict_list = f2.read().splitlines()
+    
+    for i in range(0, len(dict_list)):
+        dict_list[i] = dict_list[i].split(' ')        
+    
+    f2.close
+    
+    for i in range(0, len(dict_list)):
+        for j in range(0, len(tpm_list)):
+            if tpm_list[j][1] == dict_list[i][2]:
+                tpm_list[j][1] = dict_list[i][0] + '_' + dict_list[i][1]
+       
+    tpm_list.sort(key = lambda x: x[1])
+    
+    f3 = open('upTPM_' + markername + '.txt', 'w')
+    f3.write('marker\tdiet\tlength\teff.len\tcount\tTPM\n')
+    for i in range(0, len(tpm_list)):
+        member = tpm_list[i][0]
+        tpm_list[i][0] = tpm_list[i][1]
+        tpm_list[i][1] = member.split('/')[-2].split('_')[0]
+       
+        for j in range(0, 5):
+            f3.write(tpm_list[i][j] + '\t')
+        f3.write(tpm_list[i][5] + '\n')
+     
+    f3.close
+
+#Data:
+Markers = ['muc2', 'but1','acet1','sulfat1']  
+
+#Work:
+for i in Markers: formatting(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
